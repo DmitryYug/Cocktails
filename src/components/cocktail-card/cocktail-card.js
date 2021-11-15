211ecf0527ad45d46ca92e47f04ffb8e4cdd6f7d
Dmitriy Yuganuk <dmitriyyuganuk@gmail.com>
2021-11-15 13:45:11 +0300

    probs

--- src/components/cocktail-card/cocktail-card.js
+++ src/components/cocktail-card/cocktail-card.js
@@ -1,45 +1,38 @@
-import React, {Component} from "react";
+import React from "react";
 import CardButtons from "../card-btns/card-btns";
 import CardHeader from "../card-header/card-header";
 import CardRecipe from "../card-recipe/card-recipe";
 import './cocktail-card.css'
 
-export default class CocktailCard extends Component {
-    render () {
-        const {...header} = this.probs;
-        const elements = header.map((item) => {
-            let classNames = "card mb-3";
-            const{id, learn, ...cardContent} = item;
-            if (learn) {
-                classNames += ' learn';
-            }
-            return (
-                <div key={id} className={classNames}>
-                    <CardHeader {...cardContent}/>
-                    <CardRecipe/>
-                    <CardButtons/>
-                </div>
-            )
-        })
+
+
+
+
+const CocktailCard = ({header}) => {
+    
+    const elements = header.map((item) => {
+        let classNames = "card mb-3";
+        const{id, learn, ...cardContent} = item;
+        if (learn) {
+            classNames += ' learn'
+        }
         return (
-            <>
-                {elements}
-            </>
+            <div key={id} className={classNames}>
+                <CardHeader {...cardContent}/>
+                <CardRecipe/>
+                <CardButtons/>
+            </div>
         )
-    }
-}
+    });
 
+    return (
+        <>
+            {elements}
+        </>
+    )
+};
 
-
-// const CocktailCard = ({header}) => {
-    
-    
-//     });
-
-    
-// };
-
-// export default CocktailCard;
+export default CocktailCard;
 
 
 
